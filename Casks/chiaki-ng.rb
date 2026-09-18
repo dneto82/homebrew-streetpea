386a156ad6e5e5e898398ab20c0f73d359eb09b7
Daniel Neto <1860114+dneto82@users.noreply.github.com>
2026-09-18 17:41:15 -0300

    Drop deprecated `verified:` and migrate `postflight` to `postflight_steps`
    - Remove the deprecated `verified:` url parameter (default URL verification applies)
    - Replace the deprecated `postflight` block with the `postflight_steps` DSL, converting the `system` call to a declarative `run` step and using the `{{appdir}}` token

--- Casks/chiaki-ng.rb
+++ Casks/chiaki-ng.rb
@@ -5,8 +5,7 @@ cask "chiaki-ng" do
   sha256 arm:   "72eed7494614477dc1012b951e90ebeb212d84cdbcfa04e16adb330a022ad467",
          intel: "5611a01c2354f528b80651e428900c1233d3f6cd233ae90932589f20b8d10739"
 
-  url "https://github.com/streetpea/chiaki-ng/releases/download/v#{version}/chiaki-ng-macos_#{arch}-Release.dmg",
-      verified: "github.com/streetpea/chiaki-ng/"
+  url "https://github.com/streetpea/chiaki-ng/releases/download/v#{version}/chiaki-ng-macos_#{arch}-Release.dmg"
   name "chiaki-ng"
   desc "PlayStation remote play client next-generation"
   homepage "https://streetpea.github.io/chiaki-ng/"
@@ -17,8 +16,8 @@ cask "chiaki-ng" do
 
   app "chiaki-ng.app"
 
-  postflight do
-    system "xattr", "-d", "com.apple.quarantine", "#{appdir}/chiaki-ng.app"
+  postflight_steps do
+    run "/usr/bin/xattr", args: ["-d", "com.apple.quarantine", "{{appdir}}/chiaki-ng.app"], must_succeed: false
   end
 
   zap trash: [
